8789a12cd187df7c55c49942afcfe2e50edc54e5
+ <omuhindaaaron@gmail.com>
2024-04-13 15:34:26 +0300

    commit fetch-lab

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+const baseURL = "https://anapioficeandfire.com/api/books"
+
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  fetch(`${baseURL}`)
+  .then(res => res.json())
+  .then((json) => console.log(json));
 }
 
 function renderBooks(books) {
@@ -15,3 +18,5 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
+
